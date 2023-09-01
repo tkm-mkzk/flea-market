8b06c3e45b287e50335889430385f4eb66a08954
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2023-09-01 09:31:01 +0900

    :sparkles: Implementation of CRUD functionality

---
+++ src/items/items.controller.ts
@@ -0,0 +1,54 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { Item } from './item.model';
+import { ItemsService } from './items.service';
+import { ItemStatus } from './item-status.enum';
+
+@Controller('items')
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+
+  @Get()
+  findAll(): Item[] {
+    return this.itemsService.findAll();
+  }
+
+  @Get(':id') // items/id
+  findById(@Param('id') id: string): Item {
+    return this.itemsService.findById(id);
+  }
+
+  @Post()
+  create(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Item {
+    const item: Item = {
+      id,
+      name,
+      price,
+      description,
+      status: ItemStatus.ON_SALE,
+    };
+    return this.itemsService.create(item);
+  }
+
+  @Patch(':id')
+  updateStatus(@Param('id') id: string): Item {
+    return this.itemsService.updateStatus(id);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): void {
+    this.itemsService.delete(id);
+  }
+}

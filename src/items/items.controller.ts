0831ba84c8dd9a2ecacf4d3648a2a74ee27c42d8
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2023-09-02 13:36:48 +0900

    :sparkles: validationと例外処理

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -4,12 +4,13 @@ import {
   Delete,
   Get,
   Param,
+  ParseUUIDPipe,
   Patch,
   Post,
 } from '@nestjs/common';
 import { Item } from './item.model';
 import { ItemsService } from './items.service';
-import { ItemStatus } from './item-status.enum';
+import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('items')
 export class ItemsController {
@@ -20,35 +21,23 @@ export class ItemsController {
     return this.itemsService.findAll();
   }
 
-  @Get(':id') // items/id
-  findById(@Param('id') id: string): Item {
+  @Get(':id') // /items/id
+  findById(@Param('id', ParseUUIDPipe) id: string): Item {
     return this.itemsService.findById(id);
   }
 
   @Post()
-  create(
-    @Body('id') id: string,
-    @Body('name') name: string,
-    @Body('price') price: number,
-    @Body('description') description: string,
-  ): Item {
-    const item: Item = {
-      id,
-      name,
-      price,
-      description,
-      status: ItemStatus.ON_SALE,
-    };
-    return this.itemsService.create(item);
+  create(@Body() createItemDto: CreateItemDto): Item {
+    return this.itemsService.create(createItemDto);
   }
 
   @Patch(':id')
-  updateStatus(@Param('id') id: string): Item {
+  updateStatus(@Param('id', ParseUUIDPipe) id: string): Item {
     return this.itemsService.updateStatus(id);
   }
 
   @Delete(':id')
-  delete(@Param('id') id: string): void {
+  delete(@Param('id', ParseUUIDPipe) id: string): void {
     this.itemsService.delete(id);
   }
 }
